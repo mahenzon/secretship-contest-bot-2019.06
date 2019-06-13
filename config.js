6d08c614bfae0ee9018afcd0540ccc990df1933e
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-13 23:04:29 +0300

    Update config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 const config = {
+  token: process.env.BOT_TOKEN,
+  domain: process.env.DOMAIN,
+  hookPath: process.env.WEBHOOK_PATH || null,
+  port: process.env.PORT || 3001,
 }
 
 module.exports = config
