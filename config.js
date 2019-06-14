6e2b0a8054d646605d8d8ebbd9130c494b42ffd8
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-14 20:22:31 +0300

    Add keyboard to bot

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-const config = {
+module.exports = {
   token: process.env.BOT_TOKEN,
   domain: process.env.DOMAIN,
   hookPath: process.env.WEBHOOK_PATH || null,
   port: process.env.PORT || 3001,
 }
-
-module.exports = config
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
 const config = require('./config')
 
+
+const keyboard = Markup.inlineKeyboard([
+  Markup.loginButton('Login', `${config.domain}/login`),
+])
+
+
 const bot = new Telegraf(config.token)
 
-bot.start(({ reply }) => reply('Hello there!'))
+bot.start(ctx => ctx.reply('Hello! Here you can check all users of this bot...', Extra.markup(keyboard)))
 bot.help(({ reply }) => reply('Help message...'))
 bot.on('message', ctx => ctx.telegram.sendCopy(ctx.from.id, ctx.message))
 
 
-bot.launch({
-  webhook: {
-    port: config.port,
-    domain: config.domain,
-    hookPath: config.hookPath,
-  },
-})
+bot.launch()
+// bot.launch({
+//   webhook: {
+//     port: config.port,
+//     domain: config.domain,
+//     hookPath: config.hookPath,
+//   },
+// })
