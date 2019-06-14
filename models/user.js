d2dbb89fb53f1f60306d62e1b56e8e673b1a2230
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-14 19:17:26 +0300

    Add unique and index keys to the User model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const { Schema, model } = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new Schema({
   user_id: {
     type: String,
     required: true,
+    unique: true,
+    index: true,
   },
   first_name: String,
   last_name: String,
@@ -11,6 +14,8 @@ const userSchema = new Schema({
   language_code: String,
   profile_photo_id: String,
 })
+
+userSchema.plugin(uniqueValidator)
 
 const User = model('User', userSchema)
 module.exports = User
