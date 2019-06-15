2e460ee456425a63ccb25c7537e7951716bbfaf5
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-15 11:12:57 +0300

    Setup middleware

--- index.js
+++ index.js
@@ -29,15 +29,17 @@ db.on('error', (err) => {
 const keyboard = Markup.inlineKeyboard([
   Markup.loginButton('Login', `${config.domain}/login`),
 ])
-
+const kb = Extra.markup(keyboard)
 
 const bot = new Telegraf(config.token)
 
-bot.start(({ reply, message }) => {
+// Setup middleware to update user's info on each interacion
+bot.use(({ message }, next) => {
   addOrUpdateUser(message.from)
-  return reply('Hello! Here you can check all users of this bot...', Extra.markup(keyboard))
+  return next()
 })
 
+bot.start(({ reply }) => reply('Hello...', kb))
 bot.help(({ reply }) => reply('Help message...'))
 bot.on('message', ctx => ctx.telegram.sendCopy(ctx.from.id, ctx.message))
 
