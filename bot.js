0a5302d6e7bc18760612fc21baefb868c5e5ddfb
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-19 22:14:41 +0300

    Add i18n to messages

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 
+const I18n = require('./i18n')
 const config = require('./config')
 const { addOrUpdateUser } = require('./utils/user-actions')
 
@@ -21,13 +22,19 @@ db.on('error', (err) => {
 })
 
 
-// Create constant login keyboard
-const keyboard = Markup.inlineKeyboard([
-  Markup.loginButton('Login btn', `${config.domain}/api/login`),
-])
-const kb = Extra.markup(keyboard)
+function createLoginKb(buttonText) {
+  const keyboard = Markup.inlineKeyboard([
+    Markup.loginButton(buttonText, config.loginUrl),
+  ])
+  return Extra.markup(keyboard)
+}
+
 
 const bot = new Telegraf(config.token)
+
+bot.use(Telegraf.session())
+bot.use(I18n.middleware())
+
 
 // Setup middleware to update user's info on each interacion
 bot.use(({ message }, next) => {
@@ -35,9 +42,27 @@ bot.use(({ message }, next) => {
   return next()
 })
 
-bot.start(({ reply }) => reply('Hello...', kb))
-bot.help(({ reply }) => reply('Help message...', kb))
-bot.on('message', ctx => ctx.telegram.sendCopy(ctx.from.id, ctx.message, kb))
+bot.start(({ i18n, replyWithHTML }) => replyWithHTML(
+  [i18n.t('greeting'), i18n.t('about')].join('\n\n'),
+  createLoginKb(i18n.t('loginBtn')).webPreview(false),
+))
+bot.help(({ i18n, replyWithHTML }) => replyWithHTML(
+  [i18n.t('help'), i18n.t('about')].join('\n\n'),
+  createLoginKb(i18n.t('loginBtn')).webPreview(false),
+))
+
+// Set locales
+bot.command(['en', 'ru'], ({ i18n, reply, message }) => {
+  i18n.locale(message.text.slice(1, 3))
+  return reply(i18n.t('languageSelected'))
+})
+
+// Handle all other messages
+bot.on('message', ctx => ctx.telegram.sendCopy(
+  ctx.from.id,
+  ctx.message,
+  createLoginKb(ctx.i18n.t('loginBtn')),
+))
 
 
 // Check connection
