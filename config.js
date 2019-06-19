eb512fc3a61194a8a8d2512ad3aa7a44989fb40a
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-19 22:14:09 +0300

    Edit config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
+const domain = process.env.DOMAIN
+const rootPath = process.env.NODE_ENV === 'production' ? '/secretship-contest-2019.06/' : '/'
+const loginUrl = `${domain}${rootPath}api/login`
+
 module.exports = {
+  domain,
+  rootPath,
+  loginUrl,
   token: process.env.BOT_TOKEN,
-  domain: process.env.DOMAIN,
   hookPath: process.env.WEBHOOK_PATH || null,
   port: process.env.PORT || 3001,
   mongoConnectUri: process.env.MONGO_URI || 'mongodb://localhost/contest-bot',
